d252c35ebbc1e2c0b2c570b0a13b40416f054560
WIN-U2EPC47INN1\user <viordash@mail.ru>
2023-03-09 01:21:17 +0300

    re-add MsgpackValue<char *> tests

--- Tests/MsgpackStringValueTestsGroup.cpp
+++ Tests/MsgpackStringValueTestsGroup.cpp
@@ -8,181 +8,189 @@
 
 int main(int ac, char **av) { return RUN_ALL_TESTS(ac, av); }
 
-// TEST_GROUP(MsgpackStringValueGroup){void setup(){} void teardown(){}};
+TEST_GROUP(MsgpackStringValueGroup){void setup(){} void teardown(){}};
 
-// TEST(MsgpackStringValueGroup, MsgpackField_VeryLong_Name_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable(&container, "testString0 testString1 testString2 testString3 testString4 testString0 testString1 testString2 testString3 "
-// 										   "testStrintestString0 testString1 testString2 testString3 testString4testString0 testString1 testString2 testString3 "
-// 										   "testStrintestString0 testString1 testString2 testString3 testString4testString0 testString1 testString2 testString3 testString4g4g4");
+TEST(MsgpackStringValueGroup, MsgpackStringValue_TryParse_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testable(&container, 0, "testString");
 
-// 	CHECK_EQUAL(strlen(testable.Name), 355);
-// 	STRCMP_CONTAINS("testString0 testString1 testString2 testString3 testString4 ", testable.Name);
-// }
+	msgpack_sbuffer sbuf;
+	msgpack_packer pk;
+	msgpack_zone mempool;
+	msgpack_object deserialized;
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_TryParse_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable(&container, "testString");
+	msgpack_zone_init(&mempool, 2048);
+	msgpack_sbuffer_init(&sbuf);
+	msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write);
 
-// 	rapidjson::Document doc;
-// 	doc.Parse("{\"testString\":\"User1\"}");
-// 	CHECK_TRUE(testable.TryParse(&doc));
-// 	STRCMP_EQUAL(testable.Get(), "User1");
+	msgpack_pack_array(&pk, 1);
+	msgpack_pack_str(&pk, 5);
+	msgpack_pack_str_body(&pk, "User1", 5);
+	msgpack_unpack(sbuf.data, sbuf.size, NULL, &mempool, &deserialized);
 
-// 	doc.Parse("{\"testString\":null}");
-// 	CHECK_TRUE(testable.TryParse(&doc));
-// 	CHECK_EQUAL(testable.Get(), NULL);
-// }
+	CHECK_TRUE(testable.TryParse(&deserialized));
+	STRCMP_EQUAL(testable.Get(), "User1");
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_WriteTo_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable(&container, "testString", "1234567");
+	msgpack_zone_destroy(&mempool);
+	msgpack_sbuffer_destroy(&sbuf);
+}
 
-// 	rapidjson::Document doc;
-// 	doc.SetObject();
+TEST(MsgpackStringValueGroup, MsgpackStringValue_TryParse_When_Id_Exceed_Fields_Count_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testable(&container, 99, "testString");
 
-// 	testable.WriteToDoc(&doc);
+	msgpack_sbuffer sbuf;
+	msgpack_packer pk;
+	msgpack_zone mempool;
+	msgpack_object deserialized;
 
-// 	rapidjson::StringBuffer buffer;
-// 	rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-// 	doc.Accept(writer);
+	msgpack_zone_init(&mempool, 2048);
+	msgpack_sbuffer_init(&sbuf);
+	msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write);
 
-// 	const char *jsonStr = buffer.GetString();
+	msgpack_pack_array(&pk, 1);
+	msgpack_pack_str(&pk, 5);
+	msgpack_pack_str_body(&pk, "User1", 5);
+	msgpack_unpack(sbuf.data, sbuf.size, NULL, &mempool, &deserialized);
 
-// 	STRCMP_EQUAL(jsonStr, "{\"testString\":\"1234567\"}");
-// }
+	CHECK_FALSE(testable.TryParse(&deserialized));
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_SetValue_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable(&container, "testString");
-// 	STRCMP_EQUAL(testable.Get(), NULL);
+	msgpack_zone_destroy(&mempool);
+	msgpack_sbuffer_destroy(&sbuf);
+}
 
-// 	testable.Set("0123456789");
-// 	STRCMP_EQUAL(testable.Get(), "0123456789");
-// }
+TEST(MsgpackStringValueGroup, MsgpackStringValue_TryParse_Null_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testable(&container, 0, "testString");
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_Equals_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable1(&container, "test", "testString");
-// 	MsgpackValue<char *> testable01(&container, "test", "testString");
-// 	MsgpackValue<char *> testable001(&container, "testOtherField", "testString");
+	msgpack_sbuffer sbuf;
+	msgpack_packer pk;
+	msgpack_zone mempool;
+	msgpack_object deserialized;
 
-// 	CHECK_TRUE(testable1 == testable01);
-// 	CHECK_FALSE(testable1 != testable01);
-// 	CHECK_TRUE(testable1.Equals(&testable01));
-// 	CHECK_TRUE(testable01.Equals(&testable1));
-// 	testable01.Set("otherValue");
-// 	CHECK_TRUE(testable1 != testable01);
-// 	CHECK_FALSE(testable1 == testable01);
-// 	CHECK_FALSE(testable1.Equals(&testable01));
-// 	CHECK_FALSE(testable01.Equals(&testable1));
-// 	CHECK_FALSE(testable001 == testable1);
-// 	CHECK_FALSE(testable1 == testable001);
-// 	CHECK_FALSE(testable001.Equals(&testable1));
-// 	CHECK_FALSE(testable1.Equals(&testable001));
+	msgpack_zone_init(&mempool, 2048);
+	msgpack_sbuffer_init(&sbuf);
+	msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write);
+	msgpack_pack_array(&pk, 1);
+	msgpack_pack_nil(&pk);
+	msgpack_unpack(sbuf.data, sbuf.size, NULL, &mempool, &deserialized);
 
-// 	MsgpackValue<char *> testable2(&container, "test");
-// 	MsgpackValue<char *> testable02(&container, "test", NULL);
-// 	CHECK_TRUE(testable2 == testable02);
-// 	testable2.Set("123");
-// 	CHECK_FALSE(testable2 == testable02);
+	CHECK_TRUE(testable.TryParse(&deserialized));
+	CHECK_EQUAL(testable.Get(), NULL);
 
-// 	testable2.Set(NULL);
-// 	CHECK_TRUE(testable2 == testable02);
-// 	testable02.Set("123");
-// 	CHECK_FALSE(testable2 == testable02);
-// }
+	msgpack_zone_destroy(&mempool);
+	msgpack_sbuffer_destroy(&sbuf);
+}
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_CloneTo_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable1(&container, "test", "0123456789");
+TEST(MsgpackStringValueGroup, MsgpackStringValue_Write_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testable(&container, 0, "1234567");
 
-// 	MsgpackValue<char *> clone1(&container, "test");
+	msgpack_sbuffer sbuf;
+	msgpack_packer packer;
+	msgpack_zone mempool;
+	msgpack_object deserialized;
+	msgpack_zone_init(&mempool, 2048);
+	msgpack_sbuffer_init(&sbuf);
+	msgpack_packer_init(&packer, &sbuf, msgpack_sbuffer_write);
 
-// 	testable1.CloneTo((MsgpackValueBase *)&clone1);
-// 	testable1.Set("check the full data buffer is cloned");
-// 	STRCMP_EQUAL(clone1.Get(), "0123456789");
-// }
+	msgpack_pack_array(&packer, 1);
+	CHECK_TRUE(testable.Write(&packer));
+	msgpack_unpack(sbuf.data, sbuf.size, NULL, &mempool, &deserialized);
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_Common_TryParse_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable1(&container, "test", "0123456789");
+	msgpack_object object = deserialized.via.array.ptr[0];
+	CHECK_EQUAL(object.type, MSGPACK_OBJECT_STR);
+	STRNCMP_EQUAL(object.via.str.ptr, "1234567", object.via.str.size);
 
-// 	CHECK_FALSE(testable1.Presented());
-// 	CHECK_FALSE(testable1.IsNull());
+	msgpack_sbuffer_destroy(&sbuf);
+}
 
-// 	rapidjson::Document doc;
-// 	doc.Parse("{\"testOther\":\"01234\"}");
-// 	CHECK_TRUE(testable1.TryParse(&doc));
-// 	CHECK_FALSE(testable1.Presented());
-// 	CHECK_FALSE(testable1.IsNull());
+TEST(MsgpackStringValueGroup, MsgpackStringValue_Write_Null_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testable(&container, 0, NULL);
 
-// 	testable1.ResetToNull();
-// 	doc.Parse("{\"test\":\"01234\"}");
-// 	CHECK_TRUE(testable1.TryParse(&doc));
-// 	STRCMP_EQUAL(testable1.Get(), "01234");
-// 	CHECK_TRUE(testable1.Presented());
-// 	CHECK_FALSE(testable1.IsNull());
+	msgpack_sbuffer sbuf;
+	msgpack_packer packer;
+	msgpack_zone mempool;
+	msgpack_object deserialized;
+	msgpack_zone_init(&mempool, 2048);
+	msgpack_sbuffer_init(&sbuf);
+	msgpack_packer_init(&packer, &sbuf, msgpack_sbuffer_write);
 
-// 	doc.Parse("{\"test\":null}");
-// 	CHECK_TRUE(testable1.TryParse(&doc));
-// 	CHECK_TRUE(testable1.Presented());
-// 	CHECK_TRUE(testable1.IsNull());
-// }
+	msgpack_pack_array(&packer, 1);
+	CHECK_TRUE(testable.Write(&packer));
+	msgpack_unpack(sbuf.data, sbuf.size, NULL, &mempool, &deserialized);
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_Null_And_Empty_Value_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testDefault(&container, "testDefault");
-// 	MsgpackValue<char *> testNull(&container, "testNull", NULL);
-// 	MsgpackValue<char *> testEmpty(&container, "testEmpty", "");
+	msgpack_object object = deserialized.via.array.ptr[0];
+	CHECK_EQUAL(object.type, MSGPACK_OBJECT_NIL);
+	msgpack_sbuffer_destroy(&sbuf);
+}
 
-// 	MsgpackValue<char *> testCommonDefault(&container, "testDefault");
-// 	MsgpackValue<char *> testCommonNull(&container, "testNull", NULL);
-// 	MsgpackValue<char *> testCommonEmpty(&container, "testEmpty", "");
+TEST(MsgpackStringValueGroup, MsgpackStringValue_SetValue_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testable(&container, 0);
+	STRCMP_EQUAL(testable.Get(), NULL);
 
-// 	CHECK_EQUAL(testDefault.Get(), NULL);
-// 	CHECK_EQUAL(testNull.Get(), NULL);
-// 	STRCMP_EQUAL(testEmpty.Get(), "");
+	testable.Set("0123456789");
+	STRCMP_EQUAL(testable.Get(), "0123456789");
+}
 
-// 	CHECK_TRUE(testCommonDefault.IsNull());
-// 	CHECK_TRUE(testCommonNull.IsNull());
-// 	CHECK_FALSE(testCommonEmpty.IsNull());
-// 	CHECK_EQUAL(testCommonDefault.Get(), NULL);
-// 	CHECK_EQUAL(testCommonNull.Get(), NULL);
-// 	STRCMP_EQUAL(testCommonEmpty.Get(), "");
+TEST(MsgpackStringValueGroup, MsgpackStringValue_Equals_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testable1(&container, 0, "testString");
+	MsgpackValue<char *> testable01(&container, 0, "testString");
+	MsgpackValue<char *> testable001(&container, 1, "testString");
 
-// 	testDefault.Set("");
-// 	STRCMP_EQUAL(testDefault.Get(), "");
+	CHECK_TRUE(testable1 == testable01);
+	CHECK_FALSE(testable1 != testable01);
+	CHECK_TRUE(testable1.Equals(&testable01));
+	CHECK_TRUE(testable01.Equals(&testable1));
+	testable01.Set("otherValue");
+	CHECK_TRUE(testable1 != testable01);
+	CHECK_FALSE(testable1 == testable01);
+	CHECK_FALSE(testable1.Equals(&testable01));
+	CHECK_FALSE(testable01.Equals(&testable1));
+	CHECK_FALSE(testable001 == testable1);
+	CHECK_FALSE(testable1 == testable001);
+	CHECK_FALSE(testable001.Equals(&testable1));
+	CHECK_FALSE(testable1.Equals(&testable001));
 
-// 	testEmpty.Set(NULL);
-// 	CHECK_EQUAL(testEmpty.Get(), NULL);
+	MsgpackValue<char *> testable2(&container, 0);
+	MsgpackValue<char *> testable02(&container, 0, NULL);
+	CHECK_TRUE(testable2 == testable02);
+	testable2.Set("123");
+	CHECK_FALSE(testable2 == testable02);
 
-// 	testCommonDefault.Set("");
-// 	STRCMP_EQUAL(testCommonDefault.Get(), "");
+	testable2.Set(NULL);
+	CHECK_TRUE(testable2 == testable02);
+	testable02.Set("123");
+	CHECK_FALSE(testable2 == testable02);
+}
 
-// 	testCommonEmpty.Set(NULL);
-// 	CHECK_EQUAL(testCommonEmpty.Get(), NULL);
-// }
+TEST(MsgpackStringValueGroup, MsgpackStringValue_CloneTo_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testable1(&container, 0, "0123456789");
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_Common_Change_Presented_After_Set_Value_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable1(&container, "test");
+	MsgpackValue<char *> clone1(&container, 0);
 
-// 	CHECK_FALSE(testable1.Presented());
+	testable1.CloneTo((MsgpackValueBase *)&clone1);
+	testable1.Set("check the full data buffer is cloned");
+	STRCMP_EQUAL(clone1.Get(), "0123456789");
+}
 
-// 	testable1.Set("");
-// 	CHECK_TRUE(testable1.Presented());
-// }
+TEST(MsgpackStringValueGroup, MsgpackStringValue_Null_And_Empty_Value_Test) {
+	MsgpackFieldsContainer container;
+	MsgpackValue<char *> testDefault(&container, 0);
+	MsgpackValue<char *> testNull(&container, 0, NULL);
+	MsgpackValue<char *> testEmpty(&container, 0, "");
 
-// TEST(MsgpackStringValueGroup, MsgpackStringValue_Common_Change_IsNull_After_Set_Value_Test) {
-// 	MsgpackFieldsContainer container;
-// 	MsgpackValue<char *> testable1(&container, "test");
+	CHECK_EQUAL(testDefault.Get(), NULL);
+	CHECK_EQUAL(testNull.Get(), NULL);
+	STRCMP_EQUAL(testEmpty.Get(), "");
 
-// 	rapidjson::Document doc;
-// 	doc.Parse("{\"test\":null}");
-// 	CHECK_TRUE(testable1.TryParse(&doc));
-// 	CHECK_TRUE(testable1.IsNull());
+	testDefault.Set("");
+	STRCMP_EQUAL(testDefault.Get(), "");
 
-// 	testable1.Set("");
-// 	CHECK_FALSE(testable1.IsNull());
-// }
+	testEmpty.Set(NULL);
+	CHECK_EQUAL(testEmpty.Get(), NULL);
+}
